a5ec79e26cc46cb4c995f4ade7b83e3491162e9b
Eric Macauley <eric@macauley.me>
2017-03-01 15:25:43 -0800

    User env variable for user

--- cookbooks/install-packages/recipes/dotfiles.rb
+++ cookbooks/install-packages/recipes/dotfiles.rb
@@ -5,24 +5,24 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 # Install oh-my-zsh
-git '/home/macauley/.oh-my-zsh' do
+git '/home/' + ENV['USER_TO_ADD'] + '/.oh-my-zsh' do
   repository 'https://github.com/robbyrussell/oh-my-zsh.git'
-  user 'macauley'
+  user ENV['USER_TO_ADD']
   action :sync
 end
 
 # Install dotfiles
-git '/home/macauley/dotfiles' do
+git '/home/' + ENV['USER_TO_ADD'] + '/dotfiles' do
   repository 'https://github.com/macauley/dotfiles.git'
   revision 'master'
-  user 'macauley'
+  user ENV['USER_TO_ADD']
   action :sync
 end
 
 execute 'install_dotfiles' do
-  cwd '/home/macauley/dotfiles'
+  cwd '/home/' + ENV['USER_TO_ADD'] '/dotfiles'
   command './install'
-  user 'macauley'
+  user ENV['USER_TO_ADD']
   live_stream true
-  environment ({'HOME' => '/home/macauley'})
+  environment ({'HOME' => '/home/' + ENV['USER_TO_ADD']})
 end
